4a5d180f547bdf2d0485cba65fc64de2cb9c9cb8
Brian Bycynski <high5bri@hotmail.com>
2019-11-21 04:53:20 -0500

    started new project with same repo

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,37 @@ const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  entry: {
+    'SharePlace': './src/SharePlace.js',
+    'MyPlace': './src/MyPlace.js',
+  },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'assets', 'scripts'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist', 'assets', 'scripts'),
     publicPath: 'assets/scripts/'
   },
   devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { useBuiltIns: 'usage', corejs: { version: 3 } }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
   plugins: [new CleanPlugin.CleanWebpackPlugin()]
 };
