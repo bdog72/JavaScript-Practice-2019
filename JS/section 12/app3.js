9628ce8f0945fe20c6f40fdeb130812ebfe402e8
Brian Bycynski <high5bri@hotmail.com>
2019-11-30 04:13:04 -0500

    finished lecture 126

---
+++ JS/section 12/app3.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+console.log(12);
+
+function makeDeck() {
+  const deck = [];
+  const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+  const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A';
+
+  for (let value of values.split(',')) {
+    for (let suit of suits) {
+      deck.push({ value: value, suit: suit });
+    }
+  }
+  return deck;
+}
+
+function drawCard(deck) {
+  return deck.pop();
+}
+
+// const myDeck = makeDeck();
+// const card1 = drawCard(myDeck);
+
+const myDeck = {
+  deck: [],
+  drawnCards: [],
+  suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+  values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+  initializeDeck() {
+    // const deck = [];
+    const { deck, suits, values } = this;
+
+    for (let value of values.split(',')) {
+      for (let suit of suits) {
+        deck.push({ value, suit });
+      }
+    }
+    // return deck;
+  },
+  drawCard() {
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+    return card;
+  },
+  drawMultiple(numCards) {
+    const cards = [];
+    for (let i = 0; i < numCards; i++) {
+      cards.push(this.drawCard());
+    }
+    return cards;
+  },
+
+  // prettier-ignore
+  shuffle() {
+   const {deck} = this
+    for(let i = deck.length - 1;i>0  ;i--){
+      let j = Math.floor(Math.random()*(i+1));
+      [deck[i],deck[j]] = [deck[j],deck[i]]
+      // console.log(deck);
+    }
+  }
+};
